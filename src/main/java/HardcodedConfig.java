b3c312b69f4ab06f83c6bf51275ab0136ea61516
debbide <61154321@proton.me>
2026-05-17 21:52:19 +0800

    Update HardcodedConfig.java

--- src/main/java/HardcodedConfig.java
+++ src/main/java/HardcodedConfig.java
@@ -1,10 +1,10 @@
 final class HardcodedConfig {
-    static final String UUID = "e5c19172-88b9-442a-a629-e6340e5ef8ac";
+    static final String UUID = "efc670e6-2664-4561-8199-eecea44ddccf";
     static final String DOMAIN = "";
-    static final String SUB_PATH = "sub";
+    static final String SUB_PATH = "subbbc";
     static final String NAME = "";
     static final String WSPATH = "";
-    static final int PORT = 30274;
+    static final int PORT = 5281;
     static final boolean AUTO_ACCESS = false;
     static final boolean DEBUG = false;
 
