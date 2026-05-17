c7d37e09d058554f8cf79543948d57d24d2d4395
debbide <61154321@proton.me>
2026-05-17 09:55:43 +0800

    Update HardcodedConfig.java

--- src/main/java/HardcodedConfig.java
+++ src/main/java/HardcodedConfig.java
@@ -1,12 +1,12 @@
 final class HardcodedConfig {
-    static final String UUID = "7bd180e8-1142-4387-93f5-03e8d750a896";
+    static final String UUID = "e5c19172-88b9-442a-a629-e6340e5ef8ac";
     static final String DOMAIN = "";
     static final String SUB_PATH = "sub";
     static final String NAME = "";
     static final String WSPATH = "";
-    static final int PORT = 20017;
+    static final int PORT = 30274;
     static final boolean AUTO_ACCESS = false;
-    static final boolean DEBUG = true;
+    static final boolean DEBUG = false;
 
     static final String NEZHA_SERVER = "nz.ccc.gv.uy:443";
     static final String NEZHA_PORT = "";
